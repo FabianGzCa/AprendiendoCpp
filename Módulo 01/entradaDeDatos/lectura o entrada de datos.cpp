776169fed5d18ee186b86d40a567db639bfebc3a
Fabián González Castañeda <FabianGzCa@gmail.com>
2024-03-12 20:15:20 -0600

    Entrada de datos & Comentarios

---
+++ Módulo 01/entradaDeDatos/lectura o entrada de datos.cpp
@@ -0,0 +1,23 @@
+//Lectura o entrada de datos
+
+#include<iostream>
+
+using namespace std;
+
+/*
+
+Comentando
+Multiples
+Lineas
+
+*/
+
+int main(){
+	int numero; //Comentando este entero
+	
+	cout<<"Digita un numero: "; //Comentando esta salida de texto
+	cin>>numero;
+	
+	cout<<"\nEl numero que digito es: "<<numero;
+	return 0;
+}
